5468483f035aebfc82c976087172d402ad364a8c
mrbluedot <mrbluedot@users.noreply.github.com>
2017-02-05 13:27:10 -0500

    adding print writer

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/olekukonko/tablewriter"
 )
 
 func main() {
 	fmt.Println("Hello Go!!")
+
+	data := [][]string{
+		[]string{"cat", "Tom", "100", "13"},
+		[]string{"dog", "Pluto", "22", "13"},
+		[]string{"mouse", "Rat", "5", "13"},
+	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Type", "Name", "Power", "Age"})
+	table.AppendBulk(data)
+	table.Render()
 }
